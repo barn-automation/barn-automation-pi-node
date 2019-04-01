69f30eeca5121af4b1a00f6a0cc942a20d2e3269
Todd Sharp <toddraymondsharp@gmail.com>
2019-04-01 14:37:58 -0400

    convert from kafka to oci-streaming

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const ArduinoService = require('./service/arduino/arduino-service.js');
-const MessageProducer = require('./service/kafka/message-producer-service.js');
-const MessageConsumer = require('./service/kafka/message-consumer-service.js');
+const MessageProducer = require('./service/streaming/message-producer-service.js');
+const MessageConsumer = require('./service/streaming/message-consumer-service.js');
 const CameraService = require('./service/camera/camera-service.js');
 const GpioService = require('./service/gpio/gpio-service.js');
 const argv = require('minimist')(process.argv.slice(2));
@@ -10,16 +10,14 @@ let config = {};
 config.serialPort = "/dev/ttyACM0"
 config.gpioEnabled = false;
 config.debugArduinoSerial = false;
-config.inTopicName = "incoming";
-config.outTopicName = "outgoing";
-config.kafkaOutgoingBootstrapServer = "129.146.79.59:6667";
-config.kafkaIncomingBootstrapServer = "129.146.79.59:6667";
 config.accessToken = null;
 config.secretKey = null;
 config.storageRegion = "us-phoenix-1";
 config.storageTenancy = "toddrsharp";
 config.storageUrl = `${config.storageTenancy}.compat.objectstorage.${config.storageRegion}.oraclecloud.com`;
 config.storageBucket = "barn-captures";
+config.outgoingStreamId = "ocid1.stream.oc1.phx.aaaaaaaad5puzckqz7r6ty72c7dkw7koqbtoo2uh4g53ww5lpvd6gplcpqba";
+config.incomingStreamId = "ocid1.stream.oc1.phx.aaaaaaaatu2umvjwt7jybgxrjbkdpsjasl7xyrmzcnw62rsw3e6r6rlpakmq";
 
 config = Object.assign(config, argv);
 
@@ -39,9 +37,9 @@ if( !config.accessToken || !config.secretKey ) {
 /* end config */
 
 /* start app */
-const producer = new MessageProducer(config.outTopicName, config.kafkaOutgoingBootstrapServer);
+const producer = new MessageProducer(config.outgoingStreamId);
 const arduinoService = new ArduinoService(config.serialPort, config.debugArduinoSerial, producer);
-const consumer = new MessageConsumer(config.inTopicName, config.kafkaIncomingBootstrapServer, arduinoService);
+const consumer = new MessageConsumer(config.incomingStreamId, arduinoService);
 consumer.start();
 const cameraService = new CameraService(storageConfig, producer);
 const gpioService = new GpioService(config.gpioEnabled, cameraService);
---
+++ service/streaming/message-consumer-service.js
@@ -0,0 +1,51 @@
+const ArduinoMessage = require('../../model/arduino-message.js');
+const ConfigFileAuthProvider = require('oci-node-sdk/src/codes/recursive/auth/ConfigFileAuthProvider.js');
+const StreamingClient = require('oci-node-sdk/src/codes/recursive/client/StreamingClient.js');
+const CreateGroupCursorRequest = require('oci-node-sdk/src/codes/recursive/model/streaming/CreateGroupCursorRequest.js');
+const CreateGroupCursorDetails = require('oci-node-sdk/src/codes/recursive/model/streaming/CreateGroupCursorDetails.js');
+const GetMessagesRequest = require('oci-node-sdk/src/codes/recursive/model/streaming/GetMessagesRequest.js');
+const configAuthProvider = new ConfigFileAuthProvider(process.env.OCI_CONFIG_PATH || '~/.oci/config');
+
+module.exports = class MessageConsumer {
+
+    constructor(streamId, arduinoService) {
+        this.streamId = streamId;
+        this.arduinoService = arduinoService;
+        this.client = new StreamingClient(configAuthProvider, 'us-phoenix-1');
+    }
+
+    start() {
+        const createGroupCursorDetails = new CreateGroupCursorDetails('TRIM_HORIZON', 'group-0', null, null, null, true);
+        const createGroupCursorRequest = new CreateGroupCursorRequest(this.streamId, createGroupCursorDetails);
+        this.client.createGroupCursor(createGroupCursorRequest)
+            .then((cursorResult) => {
+                const getMessagesRequest = new GetMessagesRequest(this.streamId, cursorResult.body.value);
+                setInterval(() => {
+                    this.client.getMessages(getMessagesRequest)
+                        .then((getMessageResult) => {
+                            if( getMessageResult.body.length ) {
+                                getMessageResult.body.forEach((el, i) => {
+                                    let msg = Buffer.from(el.value, 'base64').toString('binary');
+                                    try {
+                                        msg = JSON.parse(msg);
+                                        console.log(`[INFO] Received: `, msg);
+                                        let arduinoMessage = new ArduinoMessage(msg.type, msg.message);
+                                        this.arduinoService.send(arduinoMessage);
+                                    }
+                                    catch(ex) {
+                                        console.error(ex);
+                                    }
+                                });
+                            }
+                            else {
+                                //console.log(new Date().getTime(), '<--- no messages');
+                            }
+                            getMessagesRequest.cursor = getMessageResult.headers['opc-next-cursor'];
+                        })
+                        .catch((err) => console.log(err));
+                }, 500);
+            })
+            .catch((err) => console.error(err));
+
+    }
+};
